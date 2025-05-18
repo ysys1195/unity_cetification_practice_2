37ebcd42692b7f4a46e6f0fd67d1c433ca134f61
*X4H5lQQbDM <kasumi7391@gmail.com>
2025-05-18 09:10:21 +0900

    ステージ作成

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
