caa4cc9cc4e5202ceeb4ed508be1e297730ecef1
*X4H5lQQbDM <kasumi7391@gmail.com>
2025-07-30 21:09:17 +0900

    ベストタイム記録の正解

--- Assets/Scripts/LeaderboardAlternative.cs
+++ Assets/Scripts/LeaderboardAlternative.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class LeaderboardAlternative : MonoBehaviour
 {
   public string[] formattedTimes;
-  private List<float> savedTimes = new List<float>(new float[5]);
+  private static int saveNumber = 5; // ベストタイムの数
+  private List<float> savedTimes = new List<float>(new float[saveNumber]);
+
 
   private void OnEnable()
   {
@@ -26,29 +28,13 @@ public class LeaderboardAlternative : MonoBehaviour
 
   private void GetBestTimes()
   {
-    if (PlayerPrefs.HasKey("fastTime1"))
+    for (int i = 0; i < saveNumber; i++)
     {
-      savedTimes[0] = PlayerPrefs.GetFloat("fastTime1");
-    }
-
-    if (PlayerPrefs.HasKey("fastTime2"))
-    {
-      savedTimes[1] = PlayerPrefs.GetFloat("fastTime2");
-    }
-
-    if (PlayerPrefs.HasKey("fastTime3"))
-    {
-      savedTimes[2] = PlayerPrefs.GetFloat("fastTime3");
-    }
-
-    if (PlayerPrefs.HasKey("fastTime4"))
-    {
-      savedTimes[3] = PlayerPrefs.GetFloat("fastTime4");
-    }
-
-    if (PlayerPrefs.HasKey("fastTime5"))
-    {
-      savedTimes[4] = PlayerPrefs.GetFloat("fastTime5");
+      var key = $"fastTime{i + 1}";
+      if (PlayerPrefs.HasKey(key))
+      {
+        savedTimes[i] = PlayerPrefs.GetFloat(key);
+      }
     }
 
     FormatTimesToString();
@@ -56,7 +42,7 @@ public class LeaderboardAlternative : MonoBehaviour
 
   private void FormatTimesToString()
   {
-    for (int i = 4; i >= 0; i--)
+    for (int i = saveNumber - 1; i >= 0; i--)
     {
       TimeSpan t = TimeSpan.FromSeconds(savedTimes[i]);
       formattedTimes[i] = t.ToString("m':'ss':'ff");
@@ -65,11 +51,10 @@ public class LeaderboardAlternative : MonoBehaviour
 
   private void SetBestTimes()
   {
-    PlayerPrefs.SetFloat("fastTime1", savedTimes[0]);
-    PlayerPrefs.SetFloat("fastTime2", savedTimes[1]);
-    PlayerPrefs.SetFloat("fastTime3", savedTimes[2]);
-    PlayerPrefs.SetFloat("fastTime4", savedTimes[3]);
-    PlayerPrefs.SetFloat("fastTime5", savedTimes[4]);
+    for (int i = 0; i < saveNumber; i++)
+    {
+      PlayerPrefs.SetFloat($"fastTime{i + 1}", savedTimes[i]);
+    }
 
     FormatTimesToString();
   }
@@ -79,7 +64,7 @@ public class LeaderboardAlternative : MonoBehaviour
     int scorePosition = int.MaxValue;
     bool hightScore = false;
 
-    for (int i = 4; i >= 0; i--)
+    for (int i = saveNumber - 1; i >= 0; i--)
     {
       if (RaceTimerAlternative.Instance.raceTime < savedTimes[i] || savedTimes[i] == 0)
       {
@@ -100,7 +85,7 @@ public class LeaderboardAlternative : MonoBehaviour
 
   private void CheckIfPrefsSet()
   {
-    for (int i = 0; i <= 5; i++)
+    for (int i = 0; i <= saveNumber; i++)
     {
       if (!PlayerPrefs.HasKey("fastTime" + i.ToString()))
       {
