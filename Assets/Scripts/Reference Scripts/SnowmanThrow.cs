f421a63d0b336f376b2756ffc208ae499aac2a92
*X4H5lQQbDM <kasumi7391@gmail.com>
2025-08-08 10:54:37 +0900

    見た目を変えずに雪玉投げのパフォーマンスを向上

--- Assets/Scripts/Reference Scripts/SnowmanThrow.cs
+++ Assets/Scripts/Reference Scripts/SnowmanThrow.cs
@@ -8,37 +8,39 @@ public class SnowmanThrow : MonoBehaviour
     public float throwDistance;
     public int throwSpeed;
     private bool justThown = false;
+    private GameObject target;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-       GameObject target = GameObject.Find("Player");
-       
-       float distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
+        if (justThown) return;
 
-        if (distanceToTarget < throwDistance&&justThown==false)
+        float distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
+
+        if (distanceToTarget < throwDistance)
         {
             justThown = true;
-            GameObject tempSnowBall = Instantiate(snowBall,transform.position,transform.rotation);
+            GameObject tempSnowBall = Instantiate(snowBall, transform.position, transform.rotation);
             Rigidbody tempRb = tempSnowBall.GetComponent<Rigidbody>();
-            Vector3 targetDirection =  Vector3.Normalize(target.transform.position-transform.position);
-            
+            Vector3 targetDirection = Vector3.Normalize(target.transform.position - transform.position);
+
             //Add a small throw angle
             targetDirection += new Vector3(0, 0.33f, 0);
             tempRb.AddForce(targetDirection * throwSpeed);
-            Invoke("ThrowOver", 0.1f);
+            StartCoroutine(ThrowOver());
         }
 
     }
 
-    void ThrowOver()
+    IEnumerator ThrowOver()
     {
+        yield return new WaitForSeconds(0.1f);
         justThown = false;
     }
 }
